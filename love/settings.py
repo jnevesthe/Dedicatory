cd1ff183adf04beb7d3afe6710215e916828ea8d
jnevesthe <nevesjosemar9@gmail.com>
2025-11-16 17:48:22 +0100

    Atualizando o app com novas alterações

--- love/settings.py
+++ love/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1cnnpafk0t@gp0nz7b%vd$kt)nx*^+heusth(tajm1n5p5y3)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
